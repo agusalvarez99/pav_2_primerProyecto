ea5ceca65f5c62ad6d73260956babcb57e8b5179
agusalvarez99 <agusalvarez99@hotmail.com>
2022-05-15 17:06:42 -0300

    copia al clipboard listos

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hello!')
+function copy(id_elemento) {
+  var aux = document.createElement('input');
+  aux.setAttribute('value', document.getElementById(id_elemento).innerHTML);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand('copy');
+  document.body.removeChild(aux);
+  window.alert('Copied to clipboard!');
+}
+function copyEmail() {
+  return 0;
+}
